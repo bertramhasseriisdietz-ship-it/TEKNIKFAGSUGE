60c849a78deb7f5e927c4889787f9a29e8702df8
bertramhasseriisdietz-ship-it <bertramhasseriisdietz@gmail.com>
2026-09-07 09:14:39 +0200

    Update level_1.gd

--- level_1.gd
+++ level_1.gd
@@ -14,6 +14,7 @@ func spawn_enemy():
 		var enemy = enemy_scene.instantiate()
 		get_tree().current_scene.add_child(enemy)
 		enemy.global_position = player_skib.global_position + Vector2.UP.rotated(rng.randf_range(0,1)*2*PI)*enemy_spawn_distance
+		enemy_amount += 1
 		
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
