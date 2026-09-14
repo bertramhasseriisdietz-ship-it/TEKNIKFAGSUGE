3a0745c5a6d8a293368c58194ae94067c6fad175
IHateeRainn <villadsensigurd@gmail.com>
2026-09-14 13:42:29 +0200

    Update wave_spawner.gd

--- wave_spawner.gd
+++ wave_spawner.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+signal wave_started(level: int, enemy_count: int)
+signal wave_completede(level: int)
+signal all_waves_completed
+
+# Enemy
+@export var enemy_scenes: Array[PackedScene] = []
+@export var spawn_container_path: NodePath
+
+# Størrelsen på waves
+@export var base_enemy_count: int = 5
